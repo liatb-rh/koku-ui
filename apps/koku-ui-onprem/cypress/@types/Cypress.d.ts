0daa202cc02e0da29fc585018c2e6ed1cc604cbc
Liat Berkovich <lberkovi@redhat.com>
2026-03-20 02:31:03 +0200

    load Sources MFE from Settings + Cypress E2E
    Signed-off-by: Liat Berkovich <lberkovi@redhat.com>

--- apps/koku-ui-onprem/cypress/@types/Cypress.d.ts
+++ apps/koku-ui-onprem/cypress/@types/Cypress.d.ts
@@ -7,6 +7,18 @@ declare namespace Cypress {
     loadApiInterceptors(): Chainable<void>;
 
     /**
+     * Override the sources API interceptor to return an empty list.
+     * Useful for testing the Sources empty state with type tiles.
+     */
+    interceptSourcesEmpty(): Chainable<void>;
+
+    /**
+     * Override the sources API interceptor with CRUD handlers.
+     * Also intercepts POST/PATCH/DELETE for source operations.
+     */
+    interceptSourcesCRUD(): Chainable<void>;
+
+    /**
      * Wait for the federated module to load and render
      */
     waitForFederatedModule(): Chainable<void>;
